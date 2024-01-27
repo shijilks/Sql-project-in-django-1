21568d3466d8675d25409a6737dd8fa44ad667c0
shijil <shijilks2000@gmail.com>
2024-01-27 11:46:18 +0530

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Book
+# Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
